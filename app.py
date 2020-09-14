3c12e0849963e8c85c3d2485871f97575352a0f2
tarun36rocker <tarun36rocker@gmail.com>
2020-09-14 20:10:38 +0530

    beautfied

--- app.py
+++ app.py
@@ -18,12 +18,13 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     if(prediction[0]==0):
-        output="no"
-    else:
-        output="yes"
-    
-
-    return render_template('index.html', prediction_text='Loan give : {}'.format(output))
+                output="I'm sorry , we cannot provide a loan to you "
+                pic='https://i.pinimg.com/originals/23/b8/aa/23b8aa074e2b28f26d1fd3815872e5de.png'
+                
+            else:
+                output="Your loan request has been accepted!"
+                pic='https://i.pinimg.com/originals/6c/67/40/6c6740b3ddad811f0d920a85e4d8c222.png'
+            return render_template('final.html', prediction_text='Your animal is a : {}'.format(output),pic=pic)
 
 
 if __name__ == "__main__":
