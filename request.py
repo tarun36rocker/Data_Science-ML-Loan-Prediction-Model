40cc9ca6b0a03abed088f76700edef294beb186b
tarun36rocker <tarun36rocker@gmail.com>
2020-09-25 09:02:32 +0530

    ignore

--- request.py
+++ request.py
@@ -5,4 +5,4 @@ r = requests.post(url,json={'Dependents':1, 'ApplicantIncome':6065, 'Coapplicant
 'Loan_Amount_Term':360,'Credit_History':1.0,'Gender_Female':0,'Married_No':0,'Education_Graduate':1,'Self_Employed_No':1,0
 'Property_Area_Rural':0})
 
-print(r.json())
+#print(r.json())
